5e12f4e24329bdfc152e40141bdd2c087a59ae24
Pavel Zaytsev <pavel@iberisoft.com>
2019-12-10 21:41:38 +0100

    Add project files.

---
+++ SqlExec/Program.cs
@@ -0,0 +1,42 @@
+﻿using SqlExec.Properties;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace SqlExec
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1 && File.Exists(args[0]))
+            {
+                try
+                {
+                    RunScript(args[0]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: SqlExec script-file");
+            }
+        }
+
+        private static void RunScript(string scriptFilePath)
+        {
+            using (var connection = new SqlConnection(Settings.Default.ConnectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = File.ReadAllText(scriptFilePath);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
