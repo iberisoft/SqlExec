52c1755c2fb37407ee597f02e99dfc6f1eddb350
Pavel Zaytsev <pavel@iberisoft.com>
2019-12-11 08:10:16 +0100

    Write query results to file

--- SqlExec/Program.cs
+++ SqlExec/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SqlExec.Properties;
 using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
 
@@ -9,11 +10,11 @@ namespace SqlExec
     {
         static void Main(string[] args)
         {
-            if (args.Length == 1 && File.Exists(args[0]))
+            if (args.Length > 0 && File.Exists(args[0]))
             {
                 try
                 {
-                    RunScript(args[0]);
+                    RunScript(args[0], args.Length > 1 ? args[1] : null);
                 }
                 catch (Exception ex)
                 {
@@ -22,11 +23,11 @@ namespace SqlExec
             }
             else
             {
-                Console.WriteLine("Usage: SqlExec script-file");
+                Console.WriteLine("Usage: SqlExec script-file [output-file]");
             }
         }
 
-        private static void RunScript(string scriptFilePath)
+        private static void RunScript(string scriptFilePath, string outputFilePath)
         {
             using (var connection = new SqlConnection(Settings.Default.ConnectionString))
             {
@@ -34,8 +35,40 @@ namespace SqlExec
                 using (var command = connection.CreateCommand())
                 {
                     command.CommandText = File.ReadAllText(scriptFilePath);
-                    command.ExecuteNonQuery();
+                    Console.WriteLine(command.CommandText);
+                    if (outputFilePath != null)
+                    {
+                        WriteQueryRowsToFile(command, outputFilePath);
+                    }
+                    else
+                    {
+                        command.ExecuteNonQuery();
+                    }
                 }
+            }
+        }
+
+        private static void WriteQueryRowsToFile(SqlCommand command, string outputFilePath)
+        {
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var line = string.Join(",", GetStringValues(reader));
+                        writer.WriteLine(line);
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<string> GetStringValues(SqlDataReader reader)
+        {
+            for (var i = 0; i < reader.FieldCount; ++i)
+            {
+                yield return reader.GetValue(i).ToString();
             }
         }
     }
